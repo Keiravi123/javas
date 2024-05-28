48e51212cb795954fbb1fe2173a424ae801c5324
Keiravi123 <150300716+Keiravi123@users.noreply.github.com>
2024-05-28 16:27:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Hello JavaScript is working");
+let age = 16
+const birthYear = 2007
+console.log("My age is ", age)
+let sum = age + 6
+console.log("In 6 years I will be ", sum)
+if (age <= 18 & 5=<) {
+} else {
+  console.log("I am an adult")
+}
+console.log("You are not in your twenties or late teens ")
+let isMirror = age < 18;
+console.log("Is my age less than 18? ", isMirror)
